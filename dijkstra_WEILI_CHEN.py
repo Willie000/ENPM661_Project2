d02345edcc2d7d3c2f7005d1d7371ffb74b59e85
Willie000 <121785873+Willie000@users.noreply.github.com>
2024-03-10 06:56:28 -0400

    Update dijkstra_WEILI_CHEN.py
    Update comments

--- dijkstra_WEILI_CHEN.py
+++ dijkstra_WEILI_CHEN.py
@@ -5,11 +5,12 @@ from matplotlib.animation import FuncAnimation
 import math
 import time
 
+# map setups, mapsize and clearance
 clearance = 5
 map_width = 1200
 map_height = 500
 
-# obstacles
+# map setups, obstacles
 obstacles = [
     [(100, 100), (100, 500), (175, 500), (175, 100)],
 
@@ -27,27 +28,29 @@ obstacles = [
 
 #check whether within clearance distance
 def within_clearance(x, y, polygon, clearance):
-    for i in range(len(polygon)):
-        p1 = polygon[i]
-        p2 = polygon[(i + 1) % len(polygon)]
+    for i in range(len(polygon)): #every edge
+        p1 = polygon[i] #edge point1
+        p2 = polygon[(i + 1) % len(polygon)]#edge point2
         if point_line_dist(x, y, p1[0], p1[1], p2[0], p2[1]) < clearance:
-            return True
-    return False
+            return True #robot to edge < clearance
+    return False #robot to edge > clearance
 
-#calcualte shortest distance to line
+#calcualte robot distance to nearest line
 def point_line_dist(px, py, x1, y1, x2, y2):
-    line_len = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-    line_unitvec = [(x2 - x1) / line_len, (y2 - y1) / line_len]
-    proj_length = (px - x1) * line_unitvec[0] + (py - y1) * line_unitvec[1]
-    proj_length = max(0.0, min(line_len, proj_length))
-    nearest = [x1 + line_unitvec[0] * proj_length, y1 + line_unitvec[1] * proj_length]
-    dist = ((px - nearest[0]) ** 2 + (py - nearest[1]) ** 2) ** 0.5
+    line_len = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5 #calculate edge length
+    line_unitvec = [(x2 - x1) / line_len, (y2 - y1) / line_len] #calculate unit vector
+    proj_length = (px - x1) * line_unitvec[0] + (py - y1) * line_unitvec[1] #project length
+    proj_length = max(0.0, min(line_len, proj_length)) #project point within line
+    nearest = [x1 + line_unitvec[0] * proj_length, y1 + line_unitvec[1] * proj_length] #project point
+    dist = ((px - nearest[0]) ** 2 + (py - nearest[1]) ** 2) ** 0.5 #robot to project point distance
     return dist
 
-#make sure search within maps
+#make sure search within maps, or in clearance
 def is_in_obstacle_space(x, y):
+    #check map edges
     if x < clearance or y < clearance or x > map_width - clearance or y > map_height - clearance:
         return True
+    #check obstacles edges
     for polygon in obstacles:
         if within_clearance(x, y, polygon, clearance):
             return True
@@ -57,29 +60,33 @@ def is_in_obstacle_space(x, y):
 def dijkstra(start, goal):
     actions = [(1, 0, 1), (-1, 0, 1), (0, 1, 1), (0, -1, 1), (1, 1, 1.4), (-1, -1, 1.4), (1, -1, 1.4), (-1, 1, 1.4)]
     open_list = []
-    heapq.heappush(open_list, (0, start))
-    came_from = {}
-    cost_so_far = {start: 0}
-    visited = []
+    heapq.heappush(open_list, (0, start)) #+start
+    came_from = {} #track path
+    cost_so_far = {start: 0} #record cost
+    visited = [] #close
 
     while open_list:
-        current_cost, current = heapq.heappop(open_list)
-        visited.append(current)
-
-        if current == goal:
+        current_cost, current = heapq.heappop(open_list) #min cost node
+        visited.append(current) #add to close 
+        
+        #if reached goal, finish dijkstra search
+        if current == goal: 
             break
-
+        
+        #search for all actins
         for dx, dy, move_cost in actions:
-            neighbor = (current[0] + dx, current[1] + dy)
+            neighbor = (current[0] + dx, current[1] + dy) #calculate sourding nodes
             if 0 <= neighbor[0] <= map_width and 0 <= neighbor[1] <= map_height:
                 new_cost = cost_so_far[current] + move_cost
+                #in case new nodes or lower cost
                 if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
                     if not is_in_obstacle_space(neighbor[0], neighbor[1]):
-                        cost_so_far[neighbor] = new_cost
-                        priority = new_cost
-                        heapq.heappush(open_list, (priority, neighbor))
-                        came_from[neighbor] = current
+                        cost_so_far[neighbor] = new_cost #renew cost to near nodes
+                        priority = new_cost #set as new cost
+                        heapq.heappush(open_list, (priority, neighbor)) #add to open list
+                        came_from[neighbor] = current #for track path
 
+    #track back
     current = goal
     path = []
     while current != start:
@@ -92,10 +99,11 @@ def dijkstra(start, goal):
 
 #show search process
 def animate_search(visited):
-    fig, ax = plt.subplots(figsize=(12, 5))
-    ax.set_xlim(0, map_width)
-    ax.set_ylim(0, map_height)
+    fig, ax = plt.subplots(figsize=(12, 5)) #set animate to 12:5 match map shape
+    ax.set_xlim(0, map_width) #set animate x axis
+    ax.set_ylim(0, map_height) #set animate y axis
 
+    #show obstacles
     for polygon in obstacles:
         poly = plt.Polygon(polygon, facecolor="gray", edgecolor='black')
         ax.add_patch(poly)
@@ -107,9 +115,9 @@ def animate_search(visited):
         return points,
 
     def update(frame):
-        skip = 3000
-        frame *= skip
-        visited_points = np.array(visited[:frame+1])
+        skip = 3000 #set flames skip
+        frame *= skip 
+        visited_points = np.array(visited[:frame+1]) #get visited
         points.set_offsets(visited_points)
         return points,
 
@@ -133,9 +141,9 @@ def animate_path(path):
         return line,
 
     def update(frame):
-        skip = 20
+        skip = 20 #set flames skip
         frame *= skip
-        x, y = zip(*path[:frame+1])
+        x, y = zip(*path[:frame+1]) #get path
         line.set_data(x, y)
         return line,
 
